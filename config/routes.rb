3ddbcebabdbe9c1692651b814428093e3c3bc595
George Lugo <machinelearner2334@gmail.com>
2021-11-06 12:55:47 -0700

    first part done with reviews_controller and airlines_controller w testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :airlines, param: :slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end
+
+  # this *path is routed to pages#index
+  # this will route requests that were not predefined in our API back to our
+  # index path and the reason is once we start to use 
+  # react router it will allow us to basically handle routing to our react components without interfering
+  # with our actual rails routes for our API
+  
+  get '*path', to: 'pages#index', via: :all
 end
